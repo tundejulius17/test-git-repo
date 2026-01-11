0ea12d66df52d159cfd215968996810ac69db893
tundejulius17 <tundejulius17@gmail.com>
2026-01-11 16:42:56 +0200

    updated init and function_app files

--- functions-code/blueprints/__init__.py
+++ functions-code/blueprints/__init__.py
@@ -2,4 +2,4 @@
 from .blueprint_1 import bp1
 from .blueprint_2 import bp2
 
-__all__ = ['bp1', 'bp2']
+__all__ = [bp1, bp2]
--- functions-code/function_app.py
+++ functions-code/function_app.py
@@ -1,13 +1,16 @@
 import azure.functions as func
 import logging
 
-from blueprints import __all__
+import blueprints
 
 app = func.FunctionApp(http_auth_level=func.AuthLevel.FUNCTION)
 
 # Registering all the blueprints
-import blueprints
-for blueprint_name in blueprints.__all__:
-    blueprint = getattr(blueprints, blueprint_name)
-    app.register_functions(blueprint)
+
+for bp in blueprints.__all__:
+    app.register_functions(bp)
+
+
+
+
 
